a8b166769b890fc7ce4765e099003ddbd3cdd258
kdy <ddragon0513@gmail.com>
2022-06-17 15:37:52 +0900

    test: 조회(Read) 단위 테스트 추가

--- membership/application_test.go
+++ membership/application_test.go
@@ -71,3 +71,19 @@ func TestDelete(t *testing.T) {
 
 	})
 }
+
+func TestRead(t *testing.T) {
+
+	t.Run("멤버십을 조회합니다.", func(t *testing.T) {
+
+	})
+
+	t.Run("id를 입력하지 않았을 때 예외 처리한다.", func(t *testing.T) {
+
+	})
+
+	t.Run("입력한 id가 존재하지 않을 때 예외 처리한다.", func(t *testing.T) {
+
+	})
+
+}
