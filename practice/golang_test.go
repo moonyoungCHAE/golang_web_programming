ce760bf79d75937f2242e9c49f760e7e377b9c58
준모 강 <boldfaced7@hanyang.ac.kr>
2022-06-17 21:22:12 +0900

    feat: 1주차 과제 1번 학습 테스트 진행

--- practice/golang_test.go
+++ practice/golang_test.go
@@ -3,6 +3,7 @@ package practice
 import (
 	"context"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 	"time"
 )
@@ -10,22 +11,24 @@ import (
 func TestGolang(t *testing.T) {
 
 	t.Run("string test", func(t *testing.T) {
-		//str := "Ann,Jenny,Tom,Zico"
-		//actual := "" // TODO str을 , 단위로 잘라주세요.
-		//expected := []string{"Ann","Jenny","Tom","Zico"}
-		//TODO assert 문을 활용해 actual과 expected를 비교해주세요.
+		str := "Ann,Jenny,Tom,Zico"
+		actual := strings.Split(str, ",")
+		expected := []string{"Ann", "Jenny", "Tom", "Zico"}
+		assert.ElementsMatch(t, actual, expected)
 	})
 
 	t.Run("goroutine에서 slice에 값 추가해보기", func(t *testing.T) {
 		var numbers []int
 		go func() {
 			for i := 0; i < 100; i++ {
-				// TODO numbers에 i 값을 추가해보세요.
+				numbers = append(numbers, i)
 			}
 		}()
 
-		var expected []int // actual : [0 1 2 ... 100]
-		// TODO expected를 만들어주세요.
+		var expected []int
+		for i := 0; i < 100; i++ {
+			expected = append(expected, i)
+		}
 
 		assert.ElementsMatch(t, expected, numbers)
 	})
@@ -42,9 +45,13 @@ func TestGolang(t *testing.T) {
 		inputCh := generate()
 		outputCh := make(chan int)
 		go func() {
+			defer close(outputCh)
 			for {
 				select {
-				case value := <-inputCh:
+				case value, ok := <-inputCh:
+					if !ok {
+						return
+					}
 					outputCh <- value * 10
 				}
 			}
@@ -61,7 +68,8 @@ func TestGolang(t *testing.T) {
 	t.Run("context timeout", func(t *testing.T) {
 		startTime := time.Now()
 		add := time.Second * 3
-		ctx := context.TODO() // TODO 3초후에 종료하는 timeout context로 만들어주세요.
+		ctx := context.Background()
+		ctx, _ = context.WithTimeout(ctx, time.Second*3)
 
 		var endTime time.Time
 		select {
@@ -76,7 +84,8 @@ func TestGolang(t *testing.T) {
 	t.Run("context deadline", func(t *testing.T) {
 		startTime := time.Now()
 		add := time.Second * 3
-		ctx := context.TODO() // TODO 3초후에 종료하는 timeout context로 만들어주세요.
+		ctx := context.Background()
+		ctx, _ = context.WithDeadline(ctx, time.Now().Add(time.Second*3))
 
 		var endTime time.Time
 		select {
@@ -89,9 +98,10 @@ func TestGolang(t *testing.T) {
 	})
 
 	t.Run("context value", func(t *testing.T) {
-		// context에 key, value를 추가해보세요.
-		// 추가된 key, value를 호출하여 assert로 값을 검증해보세요.
-		// 추가되지 않은 key에 대한 value를 assert로 검증해보세요.
+		ctx := context.Background()
+		ctx = context.WithValue(ctx, "my_key", "my_value")
+		assert.Equal(t, "my_value", ctx.Value("my_key"))
+		assert.Nil(t, ctx.Value("your key"))
 	})
 }
 
