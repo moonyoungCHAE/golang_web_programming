c18d3d2b4cd690906746c5c6523d61927a231d4b
kdy <ddragon0513@gmail.com>
2022-06-23 20:47:52 +0900

    feat: routes, controller 디폴트 작업

--- membership/controller.go
+++ membership/controller.go
@@ -1,1 +1,29 @@
 package membership
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+type Controller struct {
+	service Service
+}
+
+func NewController(service Service) *Controller {
+	return &Controller{service: service}
+}
+
+func (c Controller) Read(ctx echo.Context) error {
+	return nil
+}
+
+func (c Controller) Create(ctx echo.Context) error {
+	return nil
+}
+
+func (c Controller) Update(ctx echo.Context) error {
+	return nil
+}
+
+func (c Controller) Delete(ctx echo.Context) error {
+	return nil
+}
