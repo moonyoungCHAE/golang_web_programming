66a975e4985d91af3e5bc94f1a9858e46d480180
kdy <ddragon0513@gmail.com>
2022-06-23 20:48:20 +0900

    feat: main 함수 기능 추가

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"GolangLivePT01/golang_web_programming/config"
+	"GolangLivePT01/golang_web_programming/routes"
+)
 
+func main() {
+	cfg := config.GetInstance()
+	e := routes.InitializeRoutes(cfg)
+	e.Logger.Fatal(e.Start(":" + cfg.GetServicePort()))
 }
