70bedbe7eac4409e5abfc0806b828733f9b8ceee
kdy <ddragon0513@gmail.com>
2022-06-23 21:58:22 +0900

    feat: http response Code, Message 추가

--- membership/service.go
+++ membership/service.go
@@ -3,6 +3,8 @@ package membership
 import (
 	"errors"
 	"github.com/gofrs/uuid"
+	"net/http"
+	"strings"
 )
 
 var validMemberships = [3]string{"toss", "naver", "payco"}
@@ -19,70 +21,126 @@ func (app *Service) Create(request CreateRequest) (CreateResponse, error) {
 
 	randomId, err := uuid.NewGen().NewV4()
 	if err != nil {
-		return CreateResponse{}, errors.New("[create] uuid create failed")
+		return CreateResponse{Code: http.StatusInternalServerError,
+			Message: "[create] uuid create failed",
+		}, errors.New("[create] uuid create failed")
 	}
 	userId := randomId.String()
 
-	if request.UserName == "" || request.MembershipType == "" {
+	if strings.Trim(request.UserName, " ") == "" || strings.Trim(request.MembershipType, " ") == "" {
 		return CreateResponse{}, errors.New("[create] username or membership-type is not entered")
 	}
 
 	if isInvalidMembership(request.MembershipType) {
-		return CreateResponse{}, errors.New("[create] membership type is invalid")
+		return CreateResponse{Code: http.StatusBadRequest,
+			Message: "[create] membership type is invalid",
+		}, errors.New("[create] membership type is invalid")
 	}
 
 	if app.isDuplicateName(request.UserName) {
-		return CreateResponse{}, errors.New("[create] username is duplicated")
+		return CreateResponse{Code: http.StatusBadRequest,
+			Message: "[create] username is duplicated",
+		}, errors.New("[create] username is duplicated")
 	}
 
 	app.repository.Create(Membership{
 		userId, request.UserName, request.MembershipType,
 	})
 
-	return CreateResponse{userId, request.MembershipType}, nil
+	return CreateResponse{
+		Code:           http.StatusCreated,
+		Message:        "Created",
+		ID:             userId,
+		MembershipType: request.MembershipType,
+	}, nil
 }
 
 func (app *Service) Update(request UpdateRequest) (UpdateResponse, error) {
 
-	if request.ID == "" || request.UserName == "" || request.MembershipType == "" {
-		return UpdateResponse{}, errors.New("[update] ID or username, membership-type is not entered")
+	if strings.Trim(request.ID, " ") == "" || strings.Trim(request.UserName, " ") == "" || strings.Trim(request.MembershipType, " ") == "" {
+		return UpdateResponse{
+			Code:    http.StatusBadRequest,
+			Message: "[update] ID or username, membership-type is not entered",
+		}, errors.New("[update] ID or username, membership-type is not entered")
 	}
 
 	if isInvalidMembership(request.MembershipType) {
-		return UpdateResponse{}, errors.New("[update] membership type is invalid")
+		return UpdateResponse{Code: http.StatusBadRequest,
+			Message: "[update] membership type is invalid",
+		}, errors.New("[update] membership type is invalid")
 	}
 
 	if app.isDuplicateName(request.UserName) {
-		return UpdateResponse{}, errors.New("[update] username is duplicated")
+		return UpdateResponse{Code: http.StatusBadRequest,
+			Message: "[update] username is duplicated",
+		}, errors.New("[update] username is duplicated")
 	}
 
 	res := app.repository.Update(Membership{ID: request.ID, UserName: request.UserName, MembershipType: request.MembershipType})
 
 	if res.ID == "" {
-		return UpdateResponse{}, errors.New("[update] ID is not exists")
+		return UpdateResponse{
+			Code:    http.StatusNoContent,
+			Message: "[update] ID is not exists",
+		}, errors.New("[update] ID is not exists")
 	}
 
-	return UpdateResponse{res.ID, res.UserName, res.MembershipType}, nil
+	return UpdateResponse{
+		Code:           http.StatusCreated,
+		Message:        "Created",
+		ID:             res.ID,
+		UserName:       res.UserName,
+		MembershipType: res.MembershipType,
+	}, nil
 }
 
-func (app *Service) Delete(id string) error {
+func (app *Service) Delete(id string) (DeleteResponse, error) {
 
-	if id == "" {
-		return errors.New("[delete] ID is not entered")
+	if strings.Trim(id, " ") == "" {
+		return DeleteResponse{
+			Code:    http.StatusBadRequest,
+			Message: "[delete] ID is not entered",
+		}, errors.New("[delete] ID is not entered")
 	}
 	err := app.repository.Delete(id)
 
-	return err
+	if err != nil {
+		return DeleteResponse{
+			Code:    http.StatusNoContent,
+			Message: err.Error(),
+		}, err
+	}
+
+	return DeleteResponse{
+		Code:    http.StatusOK,
+		Message: "OK",
+	}, nil
 }
 
 func (app *Service) Read(id string) (ReadResponse, error) {
 
-	if id == "" {
-		return ReadResponse{}, errors.New("[read] ID is not entered")
+	if strings.Trim(id, " ") == "" {
+		return ReadResponse{
+			Code:    http.StatusBadRequest,
+			Message: "[read] ID is not entered",
+		}, errors.New("[read] ID is not entered")
 	}
 	res, err := app.repository.ReadById(id)
 
-	return ReadResponse{res.ID, res.UserName, res.MembershipType}, err
+	if err != nil {
+		return ReadResponse{
+			Code:    http.StatusNoContent,
+			Message: err.Error(),
+		}, nil
+	}
+
+	return ReadResponse{
+		Code:           http.StatusOK,
+		Message:        "OK",
+		ID:             res.ID,
+		UserName:       res.UserName,
+		MembershipType: res.MembershipType,
+	}, nil
 }
 
 // isDuplicateName returns a bool value whether if username is duplicated or not
