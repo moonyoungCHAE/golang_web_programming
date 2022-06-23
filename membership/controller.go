855d6a75f79471dca8e7432c957ea4b8fe12d844
kdy <ddragon0513@gmail.com>
2022-06-23 22:12:39 +0900

    feat: controller 기능 구현

--- membership/controller.go
+++ membership/controller.go
@@ -2,6 +2,7 @@ package membership
 
 import (
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 type Controller struct {
@@ -13,17 +14,35 @@ func NewController(service Service) *Controller {
 }
 
 func (c Controller) Read(ctx echo.Context) error {
-	return nil
+	id := ctx.Param("id")
+	res, _ := c.service.Read(id)
+
+	return ctx.JSON(res.Code, res)
 }
 
 func (c Controller) Create(ctx echo.Context) error {
-	return nil
+	var req CreateRequest
+	err := ctx.Bind(&req)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, CreateResponse{Message: "invalid_request_format"})
+	}
+	res, _ := c.service.Create(req)
+
+	return ctx.JSON(res.Code, res)
 }
 
 func (c Controller) Update(ctx echo.Context) error {
-	return nil
+	var req UpdateRequest
+	err := ctx.Bind(&req)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, UpdateResponse{Message: "invalid_request_format"})
+	}
+	res, _ := c.service.Update(req)
+	return ctx.JSON(res.Code, res)
 }
 
 func (c Controller) Delete(ctx echo.Context) error {
-	return nil
+	id := ctx.Param("id")
+	res, _ := c.service.Delete(id)
+	return ctx.JSON(res.Code, res)
 }
