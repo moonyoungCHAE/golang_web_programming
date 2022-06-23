5f184393e0c46cfed4ef8d3a189948a82284f78c
kdy <ddragon0513@gmail.com>
2022-06-23 20:47:52 +0900

    feat: routes, controller 디폴트 작업

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,18 @@
 package routes
+
+import (
+	"GolangLivePT01/golang_web_programming/config"
+	"github.com/labstack/echo/v4"
+)
+
+func InitializeRoutes(cfg *config.AppConfig) *echo.Echo {
+
+	controller := cfg.GetController()
+	memberships := cfg.GetGroup().Group("/memberships")
+	memberships.POST("/", controller.Create)
+	memberships.GET("/:id", controller.Read)
+	memberships.PUT("/:id", controller.Update)
+	memberships.DELETE("/:id", controller.Delete)
+
+	return cfg.GetEcho()
+}
