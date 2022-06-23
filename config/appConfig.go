78c2a33acb8a255d15c2f367824b0b4102504f91
kdy <ddragon0513@gmail.com>
2022-06-23 20:46:45 +0900

    feat: AppConfig 초기화 기능 추가

--- config/appConfig.go
+++ config/appConfig.go
@@ -1,1 +1,55 @@
 package config
+
+import (
+	"GolangLivePT01/golang_web_programming/membership"
+	"github.com/labstack/echo/v4"
+	"sync"
+)
+
+var instance *AppConfig
+var once sync.Once
+
+type AppConfig struct {
+	servicePort string
+	echo        *echo.Echo
+	group       *echo.Group
+	controller  *membership.Controller
+}
+
+func GetInstance() *AppConfig {
+	once.Do(func() {
+		controller := initApplication()
+		echo := echo.New()
+		group := echo.Group("/api/v2")
+		instance = &AppConfig{
+			servicePort: "8080",
+			echo:        echo,
+			group:       group,
+			controller:  controller,
+		}
+	})
+	return instance
+}
+
+func initApplication() *membership.Controller {
+	data := map[string]membership.Membership{}
+	service := membership.NewService(*membership.NewRepository(data))
+	controller := membership.NewController(*service)
+	return controller
+}
+
+func (conf *AppConfig) GetServicePort() string {
+	return conf.servicePort
+}
+
+func (conf *AppConfig) GetEcho() *echo.Echo {
+	return conf.echo
+}
+
+func (conf *AppConfig) GetGroup() *echo.Group {
+	return conf.group
+}
+
+func (conf *AppConfig) GetController() membership.Controller {
+	return *conf.controller
+}
