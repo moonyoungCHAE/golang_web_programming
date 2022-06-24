03aaa73f993c7bc90644e9515b9df14f618a1104
kdy <ddragon0513@gmail.com>
2022-06-24 17:07:50 +0900

    feat: Echo 미들웨어 기능 (Web Logging) 추가

--- routes/routes.go
+++ routes/routes.go
@@ -2,13 +2,26 @@ package routes
 
 import (
 	"GolangLivePT01/golang_web_programming/config"
+	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"strings"
 )
 
 func InitializeRoutes(cfg *config.AppConfig) *echo.Echo {
 
 	controller := cfg.GetController()
 	memberships := cfg.GetGroup().Group("/memberships")
+
+	memberships.Use(middleware.BodyDump(func(c echo.Context, reqBody []byte, resBody []byte) {
+		reqStr := strings.Trim(string(reqBody), "\n")
+		resStr := strings.Trim(string(resBody), "\n")
+		c.Logger().Output().Write([]byte(fmt.Sprintf("RequestBody:[%s]\nResponseBody:[%s]\n\n", reqStr, resStr)))
+	}))
+	memberships.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "URI:[${uri}], Method:[${method}], ResponseHttpStatusCode:[${status}]\n",
+	}))
+
 	memberships.POST("", controller.Create)
 	memberships.GET("", controller.ReadAll)
 	memberships.GET("/:id", controller.Read)
