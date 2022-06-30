d0bcbed3784b00bd9a7821e767d1ab78bbf236d8
준모 강 <boldfaced7@hanyang.ac.kr>
2022-06-30 16:10:33 +0900

    feat: controller.go 생성

---
+++ app/membership/controller.go
@@ -0,0 +1,40 @@
+package membership
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+type Controller struct {
+	service Service
+}
+
+func NewController(service Service) *Controller {
+	return &Controller{service: service}
+}
+
+func (controller Controller) Create(c echo.Context) error {
+	var req CreateRequest
+	err := c.Bind(&req)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "invalid_request_format")
+	}
+	res, err := controller.service.Create(req)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	return c.JSON(res.Code, res)
+}
+
+func (controller Controller) Update(c echo.Context) error {
+	var req UpdateRequest
+	err := c.Bind(&req)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "invalid_request_format")
+	}
+	res, err := controller.service.Update(req)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	return c.JSON(res.Code, res)
+}
