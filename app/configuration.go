1c28f2966f65cfb1bc6cbbfae3eabfdb7ef9d455
준모 강 <boldfaced7@hanyang.ac.kr>
2022-07-01 16:30:27 +0900

    feat: 로깅을 위한 미들웨어 추가

--- app/configuration.go
+++ app/configuration.go
@@ -3,6 +3,8 @@ package app
 import (
 	"github.com/boldfaced7/golang_web_programming/app/membership"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log"
 )
 
 type Config struct {
@@ -20,8 +22,8 @@ func DefaultConfig() *Config {
 
 func NewEcho(config Config) *echo.Echo {
 	e := echo.New()
-
 	controller := config.Controller
+	LoggingMiddleware(e)
 
 	e.POST("/memberships", controller.Create)
 	e.PUT("/memberships", controller.Update)
@@ -29,6 +31,19 @@ func NewEcho(config Config) *echo.Echo {
 	e.DELETE("/memberships", controller.Delete)
 	// GET /memberships?offset=1,limit=3
 	e.GET("/memberships", controller.GetSome)
-
 	return e
+}
+
+func LoggingMiddleware(e *echo.Echo) {
+	e.Use(middleware.BodyDump(func(c echo.Context, reqBody []byte, resBody []byte) {
+		log.Printf("URI=%v", c.Request().RequestURI)
+		log.Printf("Method=%v", c.Request().Method)
+		if string(reqBody) != "" {
+			log.Printf("Request Body=%v", string(reqBody))
+		}
+		log.Printf("Status Code=%v", c.Response().Status)
+		if string(resBody) != "" {
+			log.Printf("Response Body=%v", string(resBody))
+		}
+	}))
 }
