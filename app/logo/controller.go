3d31d18e1a58b8388272c9a3861a1a2d08685fda
준모 강 <boldfaced7@hanyang.ac.kr>
2022-07-02 13:32:40 +0900

    feat: Etag 기능 구현

---
+++ app/logo/controller.go
@@ -0,0 +1,28 @@
+package logo
+
+import (
+	"crypto/md5"
+	"fmt"
+	"github.com/labstack/echo"
+	"os"
+)
+
+type Controller struct {
+}
+
+func NewController() *Controller {
+	return &Controller{}
+}
+
+func (controller Controller) Get(c echo.Context) error {
+	url := "./assets/memebership.png"
+	file, err := os.Stat(url)
+	if err != nil {
+		return echo.ErrInternalServerError
+	}
+
+	modifiedTime := file.ModTime()
+	etag := fmt.Sprintf("%x", md5.Sum([]byte(modifiedTime.String())))
+	c.Response().Header().Set("ETag", etag)
+	return c.File(url)
+}
