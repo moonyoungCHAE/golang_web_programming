2e6d3a1f264c501e09fa41b03de53b30dd7e8a16
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-07-13 23:26:33 +0900

    test: split, go-routine 테스트 추가

--- practice/golang_learning_test.go
+++ practice/golang_learning_test.go
@@ -1,97 +1,114 @@
 package practice
 
 import (
-	"context"
-	"github.com/stretchr/testify/assert"
+	"strings"
+	"sync"
 	"testing"
-	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // golang 학습 테스트
 func TestGolang(t *testing.T) {
 	t.Run("string test", func(t *testing.T) {
-		//str := "Ann,Jenny,Tom,Zico"
-		//actual := "" // TODO str을 , 단위로 잘라주세요.
-		//expected := []string{"Ann","Jenny","Tom","Zico"}
+		str := "Ann,Jenny,Tom,Zico"
+		// TODO str을 , 단위로 잘라주세요.
+		actual := strings.Split(str, ",")
+		expected := []string{"Ann", "Jenny", "Tom", "Zico"}
 		//TODO assert 문을 활용해 actual과 expected를 비교해주세요.
-	})
-
-	t.Run("goroutine에서 slice에 값 추가해보기", func(t *testing.T) {
-		var numbers []int
-		for i := 0; i < 100; i++ {
-			go func() {
-				// TODO numbers에 i 값을 추가해보세요.
-			}()
-		}
-
-		var expected []int // actual : [0 1 2 ... 99]
-		// TODO expected를 만들어주세요.
-		assert.ElementsMatch(t, expected, numbers)
-	})
-
-	t.Run("fan out, fan in", func(t *testing.T) {
-		/*
-			TODO 주어진 코드를 수정해서 테스트가 통과하도록 해주세요!
-
-			- inputCh에 1, 2, 3 값을 넣는다.
-			- inputCh로 부터 값을 받아와, value * 10 을 한 후 outputCh에 값을 넣어준다.
-			- outputCh에서 읽어온 값을 비교한다.
-		*/
-
-		inputCh := generate()
-		outputCh := make(chan int)
-		go func() {
-			for {
-				select {
-				case value := <-inputCh:
-					outputCh <- value * 10
-				}
-			}
-		}()
-
-		var actual []int
-		for value := range outputCh {
-			actual = append(actual, value)
-		}
-		expected := []int{10, 20, 30}
 		assert.Equal(t, expected, actual)
 	})
 
-	t.Run("context timeout", func(t *testing.T) {
-		startTime := time.Now()
-		add := time.Second * 3
-		ctx := context.TODO() // TODO 3초후에 종료하는 timeout context로 만들어주세요.
+	t.Run("goroutine에서 slice에 값 추가해보기", func(t *testing.T) {
+		var numbers [100]int
+		var wg sync.WaitGroup
+		wg.Add(100)
+		for i := 0; i < 100; i++ {
+			// 익명함수로 파라미터 i를 전달해야 모든 숫자가 추가될수 있다.
+			go func(i int) {
+				// TODO numbers에 i 값을 추가해보세요.
+				//numbers = append(numbers, i) <- 완전히 랜덤값이 중복으로 들어오므로 fail
+				numbers[i] = i
 
-		var endTime time.Time
-		select {
-		case <-ctx.Done():
-			endTime = time.Now()
-			break
+				//Done이 될때마다 Add 카운트는 -1
+				wg.Done()
+			}(i)
 		}
+		// 0이 될때까지 기다림
+		wg.Wait()
 
-		assert.True(t, endTime.After(startTime.Add(add)))
-	})
-
-	t.Run("context deadline", func(t *testing.T) {
-		startTime := time.Now()
-		add := time.Second * 3
-		ctx := context.TODO() // TODO 3초후에 종료하는 timeout context로 만들어주세요.
-
-		var endTime time.Time
-		select {
-		case <-ctx.Done():
-			endTime = time.Now()
-			break
+		// actual : [0 1 2 ... 99]
+		var expected []int = make([]int, 100)
+		// TODO expected를 만들어주세요.
+		for i := 0; i < 100; i++ {
+			expected[i] = i
 		}
-
-		assert.True(t, endTime.After(startTime.Add(add)))
+		assert.ElementsMatch(t, expected, numbers)
 	})
 
-	t.Run("context value", func(t *testing.T) {
-		// context에 key, value를 추가해보세요.
-		// 추가된 key, value를 호출하여 assert로 값을 검증해보세요.
-		// 추가되지 않은 key에 대한 value를 assert로 검증해보세요.
-	})
+	// t.Run("fan out, fan in", func(t *testing.T) {
+	// 	/*
+	// 		TODO 주어진 코드를 수정해서 테스트가 통과하도록 해주세요!
+
+	// 		- inputCh에 1, 2, 3 값을 넣는다.
+	// 		- inputCh로 부터 값을 받아와, value * 10 을 한 후 outputCh에 값을 넣어준다.
+	// 		- outputCh에서 읽어온 값을 비교한다.
+	// 	*/
+
+	// 	inputCh := generate()
+	// 	outputCh := make(chan int)
+	// 	go func() {
+	// 		for {
+	// 			select {
+	// 			case value := <-inputCh:
+	// 				outputCh <- value * 10
+	// 			}
+	// 		}
+	// 	}()
+
+	// 	var actual []int
+	// 	for value := range outputCh {
+	// 		actual = append(actual, value)
+	// 	}
+	// 	expected := []int{10, 20, 30}
+	// 	assert.Equal(t, expected, actual)
+	// })
+
+	// t.Run("context timeout", func(t *testing.T) {
+	// 	startTime := time.Now()
+	// 	add := time.Second * 3
+	// 	ctx := context.TODO() // TODO 3초후에 종료하는 timeout context로 만들어주세요.
+
+	// 	var endTime time.Time
+	// 	select {
+	// 	case <-ctx.Done():
+	// 		endTime = time.Now()
+	// 		break
+	// 	}
+
+	// 	assert.True(t, endTime.After(startTime.Add(add)))
+	// })
+
+	// t.Run("context deadline", func(t *testing.T) {
+	// 	startTime := time.Now()
+	// 	add := time.Second * 3
+	// 	ctx := context.TODO() // TODO 3초후에 종료하는 timeout context로 만들어주세요.
+
+	// 	var endTime time.Time
+	// 	select {
+	// 	case <-ctx.Done():
+	// 		endTime = time.Now()
+	// 		break
+	// 	}
+
+	// 	assert.True(t, endTime.After(startTime.Add(add)))
+	// })
+
+	// t.Run("context value", func(t *testing.T) {
+	// 	// context에 key, value를 추가해보세요.
+	// 	// 추가된 key, value를 호출하여 assert로 값을 검증해보세요.
+	// 	// 추가되지 않은 key에 대한 value를 assert로 검증해보세요.
+	// })
 }
 
 func generate() <-chan int {
