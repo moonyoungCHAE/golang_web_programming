3140b74e46de5c76be96344231b0e66c338d3603
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-07-14 23:05:39 +0900

    feat: 저장소에 중복된 이름 식별 로직추가

--- internal/application.go
+++ internal/application.go
@@ -1,5 +1,10 @@
 package internal
 
+import (
+	"log"
+	"strconv"
+)
+
 type Application struct {
 	repository Repository
 }
@@ -9,7 +14,30 @@ func NewApplication(repository Repository) *Application {
 }
 
 func (app *Application) Create(request CreateRequest) (CreateResponse, error) {
-	return CreateResponse{"1", "naver"}, nil
+	membershipBuilder := NewMembershipGenerator()
+	member_repository := app.repository
+	member_count := len(member_repository.data)
+
+	id := strconv.Itoa(member_count + 1)
+	log.Println(id)
+
+	membershipBuilder.
+		SetID(id).
+		SetUserName(request.UserName).
+		SetMembershipType(request.MembershipType)
+
+	membership, err := membershipBuilder.GetMembership()
+	if err != nil {
+		return CreateResponse{}, err
+	}
+
+	_, err = app.repository.
+		AddRepository(*membership)
+	if err != nil {
+		return CreateResponse{}, err
+	}
+
+	return CreateResponse{membership.ID, membership.MembershipType}, nil
 }
 
 func (app *Application) Update(request UpdateRequest) (UpdateResponse, error) {
