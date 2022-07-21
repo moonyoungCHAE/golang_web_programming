6925e9dfe7b69d425a04af05b364554188aea76b
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-07-21 22:40:57 +0900

    refactor: DTO Json Parsing

--- internal/dto.go
+++ internal/dto.go
@@ -1,35 +1,35 @@
 package internal
 
 type CreateRequest struct {
-	UserName       string
-	MembershipType string
+	UserName       string `json:"user_name"`
+	MembershipType string `json:"membership_type"`
 }
 
 type CreateResponse struct {
-	ID             string
-	MembershipType string
+	ID             string `json:"id"`
+	MembershipType string `json:"membership_type"`
 }
 
 type UpdateRequest struct {
-	ID             string
-	UserName       string
-	MembershipType string
+	ID             string `json:"id"`
+	UserName       string `json:"user_name"`
+	MembershipType string `json:"membership_type"`
 }
 
 type UpdateResponse struct {
-	ID             string
-	UserName       string
-	MembershipType string
+	ID             string `json:"id"`
+	UserName       string `json:"user_name"`
+	MembershipType string `json:"membership_type"`
 }
 
 type DeleteRequest struct {
-	ID string
+	ID string `json:"id"`
 }
 
 type DeleteResponse struct {
-	ID             string
-	UserName       string
-	MembershipType string
+	ID             string `json:"id"`
+	UserName       string `json:"user_name"`
+	MembershipType string `json:"membership_type"`
 }
 
 type GetResponse struct {
