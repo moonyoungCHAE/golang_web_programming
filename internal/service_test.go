102e3128770af4c836461f094c3f28ca3b70b6a3
Michael Youngchan CHO <michael.yc.cho@gmail.com>
2022-07-21 23:29:20 +0900

    test: Update Test코드

---
+++ internal/service_test.go
@@ -0,0 +1,147 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateMembership(t *testing.T) {
+	t.Run("멤버십을 생성한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+		req := CreateRequest{"jenny", "naver"}
+		res, err := app.Create(req)
+
+		assert.Nil(t, err)
+		assert.NotEmpty(t, res.ID)
+		assert.Equal(t, req.MembershipType, res.MembershipType)
+	})
+
+	t.Run("이미 등록된 사용자 이름이 존재할 경우 실패한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+		app.Create(CreateRequest{"jenny", "naver"})
+		_, err := app.Create(CreateRequest{"jenny", "payco"})
+
+		assert.NotNil(t, err)
+		assert.Equal(t, ErrUserAlreadyExists, err)
+	})
+
+	t.Run("사용자 이름을 입력하지 않은 경우 실패한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+		_, err := app.Create(CreateRequest{"", "payco"})
+		assert.NotNil(t, err)
+		assert.Equal(t, ErrUserNameIsRequired, err)
+	})
+
+	t.Run("멤버십 타입을 입력하지 않은 경우 실패한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+		_, err := app.Create(CreateRequest{"jenny", ""})
+		assert.NotNil(t, err)
+		assert.Equal(t, ErrMembershipTypeIsRequired, err)
+	})
+
+	t.Run("naver/toss/payco 이외의 타입을 입력한 경우 실패한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+		_, err := app.Create(CreateRequest{"jenny", "paybook"})
+		assert.NotNil(t, err)
+		assert.Equal(t, ErrInvalidMembershipType, err)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("membership 정보를 갱신한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+
+		res, err := app.Create(CreateRequest{
+			UserName:       "jenny",
+			MembershipType: "payco",
+		})
+
+		req := UpdateRequest{
+			ID:             res.ID,
+			UserName:       "jenny",
+			MembershipType: "naver",
+		}
+
+		_, err = app.Update(req)
+		membershipFromData, _ := app.repository.data[res.ID]
+
+		assert.Equal(t, "naver", membershipFromData.MembershipType)
+		assert.Nil(t, err)
+	})
+
+	t.Run("수정하려는 사용자의 이름이 이미 존재하는 사용자 이름이라면 예외 처리한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+
+		app.Create(CreateRequest{
+			UserName:       "jenny",
+			MembershipType: "payco",
+		})
+
+		_, err := app.Update(UpdateRequest{
+			ID:             "update-1",
+			UserName:       "jenny",
+			MembershipType: "payco",
+		})
+
+		if assert.Error(t, err) {
+			assert.Equal(t, ErrUserAlreadyExists, err)
+		}
+	})
+
+	t.Run("멤버십 아이디를 입력하지 않은 경우, 예외 처리한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+
+		_, err := app.Update(UpdateRequest{
+			ID:             "",
+			UserName:       "jenny",
+			MembershipType: "payco",
+		})
+
+		if assert.Error(t, err) {
+			assert.Equal(t, ErrUpdateRequestIsRequired, err)
+		}
+	})
+
+	t.Run("사용자 이름을 입력하지 않은 경우, 예외 처리한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+
+		_, err := app.Update(UpdateRequest{
+			ID:             "update-2",
+			UserName:       "",
+			MembershipType: "payco",
+		})
+
+		if assert.Error(t, err) {
+			assert.Equal(t, ErrUpdateRequestIsRequired, err)
+		}
+	})
+
+	t.Run("멤버쉽 타입을 입력하지 않은 경우, 예외 처리한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+
+		_, err := app.Update(UpdateRequest{
+			ID:             "update-3",
+			UserName:       "jenny",
+			MembershipType: "",
+		})
+
+		if assert.Error(t, err) {
+			assert.Equal(t, ErrUpdateRequestIsRequired, err)
+		}
+	})
+
+	t.Run("주어진 멤버쉽 타입이 아닌 경우, 예외 처리한다.", func(t *testing.T) {
+		app := NewService(*NewRepository(map[string]Membership{}))
+
+		_, err := app.Update(UpdateRequest{
+			ID:             "update-4",
+			UserName:       "jenny",
+			MembershipType: "kakao",
+		})
+
+		if assert.Error(t, err) {
+			assert.Equal(t, ErrInvalidMembershipType, err)
+		}
+	})
+}
